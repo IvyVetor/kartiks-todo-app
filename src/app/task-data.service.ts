db2f2bfa6c06648b8dbb7ccbdaa875458f111576
ivy.vetor@asynchrony.com <ivy.vetor@asynchrony.com>
2019-11-25 08:00:28 -0500

    fixing local storage issue

--- src/app/task-data.service.ts
+++ src/app/task-data.service.ts
@@ -1,12 +1,13 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {Task} from './task';
+import {LOCAL_STORAGE, WebStorageService} from "angular-webstorage-service";
 
 @Injectable()
 export class TaskDataService {
   placeholderId = 0;
   tasks: Task[] = [];
 
-  constructor() {
+  constructor(@Inject(LOCAL_STORAGE) private storage: WebStorageService) {
   }
 
   createNewTask(task: Task): TaskDataService {
@@ -18,19 +19,25 @@ export class TaskDataService {
     }
     task.title = task.title.trim();
     this.tasks.push(task);
+    this.storage.set(task.id.toString(), task);
+    this.storage.set('tasks', this.tasks);
     return this;
   }
 
   getAllTasks(): Task[] {
+    this.tasks = this.storage.get('tasks');
     return this.tasks;
   }
 
   toggleComplete(task: Task) {
+    console.log('these tasks BEFORE updating', this.tasks);
     task.complete = !task.complete;
+    console.log('these tasks AFTER updating', this.tasks);
+    this.storage.set('tasks', this.tasks);
   }
 
   clearCompletedTasks() {
-    this.tasks = this.tasks.filter(task => !task.complete);
-    return this.tasks;
+    this.tasks = this.storage.get('tasks').filter(task => !task.complete);
+    return this.tasks.filter(task => !task.complete);
   }
 }
